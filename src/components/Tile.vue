ec5cad28c00d6c9e52fa53c78efe7c2c0e98267b
y-kawamura <tetora.y@gmail.com>
2020-04-06 09:19:32 +0900

    Add slide animation and Modify tile style

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -3,8 +3,12 @@
     <div
       v-if="tile"
       class="tile"
-      :class="[{ popup: isPopup }, `tile-${tile.value}`]"
-      @animationend="animated = true"
+      :class="[
+        { popup: isPopup, 'popup-merge': isPopupMerge },
+        `tile-${tile.value}`,
+        `${slideInAnimation}`,
+      ]"
+      @animationend="onAnimationEnd"
     >
       <span>{{ tile.value }}</span>
     </div>
@@ -18,45 +22,69 @@ export default {
   props: ['tile'],
   data() {
     return {
-      animated: false,
+      donePopupAnimation: false,
+      doneSlideAnimation: false,
     };
   },
   computed: {
     ...mapState(['moveCount']),
     isPopup() {
-      console.log(
-        this.tile.x,
-        this.tile.y,
-        !this.animated,
-        !this.tile.previous,
-        this.tile.merged
+      console.log('previous', !this.tile.previous)
+      return !this.donePopupAnimation && !this.tile.previous;
+    },
+    // Note: animation is triggered after slide animation is done
+    isPopupMerge() {
+      return this.tile.merged;
+    },
+    isSlide() {
+      return (
+        !this.doneSlideAnimation &&
+        this.tile.previous &&
+        (this.tile.previous.x !== this.tile.x ||
+          this.tile.previous.y !== this.tile.y)
       );
-
-      return !this.animated && (!this.tile.previous || this.tile.merged);
+    },
+    slideInAnimation() {
+      if (!this.isSlide) {
+        return '';
+      }
+      const x = this.tile.previous.x - this.tile.x;
+      const y = this.tile.previous.y - this.tile.y;
+      return `slide_${x}x_${y}y`;
     },
   },
   methods: {
-    removeClass(item) {
-      item.highlight = false;
+    onAnimationEnd(e) {
+      if (e.animationName === 'popup') {
+        this.donePopupAnimation = true;
+      }
+      if (e.animationName.match(/^slide/)) {
+        this.doneSlideAnimation = true;
+      }
     },
   },
   watch: {
+    // to restart animation
     moveCount() {
-      this.animated = false;
+      this.donePopupAnimation = false;
+      this.doneSlideAnimation = false;
     },
   },
 };
 </script>
 
 <style lang="scss">
+$cellSize: 5rem;
+$margin: 0.5rem;
+$popup-animation-time: 200ms;
+$move-animation-time: 100ms;
+
 .tile-outer {
-  width: 80px;
-  height: 80px;
-  margin: 0.5rem;
+  width: $cellSize;
+  height: $cellSize;
+  margin: $margin;
   border-radius: 0.5rem;
-
-
-  color: eee;
+  background-color: #393a41;
 }
 
 .tile {
@@ -67,21 +95,70 @@ export default {
   justify-content: center;
   align-items: center;
   font-size: 2rem;
+  color: #eee;
 }
 
-.tile-white {
-  background-color: #fcfcfc;
+.popup {
+  transform: scale(0);
+  animation: popup $popup-animation-time $move-animation-time;
+}
+.popup-merge {
+  animation: popup $popup-animation-time;
+}
+@keyframes popup {
+  0% {
+    transform: scale(0.5);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 
+@function distance($value) {
+  @return $cellSize * $value + $margin * $value;
+}
+
+@for $x from -3 through 3 {
+  .slide_#{$x}x_0y {
+    animation: slide_#{$x}x_0y $move-animation-time;
+  }
+  @keyframes slide_#{$x}x_0y {
+    0% {
+      transform: translate(distance($x), 0);
+    }
+    100% {
+      transform: translate(0, 0);
+    }
+  }
+}
+
+@for $y from -3 through 3 {
+  .slide_0x_#{$y}y {
+    animation: slide_0x_#{$y}y $move-animation-time;
+  }
+  @keyframes slide_0x_#{$y}y {
+    0% {
+      transform: translate(0, distance($y));
+    }
+    100% {
+      transform: translate(0, 0);
+    }
+  }
+}
+
+// color table
 .tile-2 {
-  background-color: #BCD2E8;
-  color:#000;
+  background-color: #bcd2e8;
+  color: #000;
 }
 .tile-4 {
-  background-color: #91BAD6;
+  background-color: #91bad6;
 }
 .tile-8 {
-  background-color: #73A5C6;
+  background-color: #73a5c6;
 }
 .tile-16 {
   background-color: #57a0d3;
@@ -109,22 +186,4 @@ export default {
   background-color: #1b63e9;
   font-size: 1.5rem;
 }
-
-.popup {
-  animation: popup 0.5s;
-}
-
-@keyframes popup {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.2);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-
-// #4b59f1
 </style>
